dc9e4175a8d67e5df2ad3830bf3df693d1554b97
Jerry Linder <jerryl1089@gmail.com>
2014-10-05 02:42:07 -0500

    latest webdev branch

--- Python/httpserver.py
+++ Python/httpserver.py
@@ -1,14 +1,21 @@
 import BaseHTTPServer
+import db_Connect
 
 server_host = 'localhost'
 server_port = 80
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_HEAD(s):
+    def __init__(self):
+        print "RequestHandler constructor initialized"
+        self.db = db_Connect(1)
+        super(RequestHandler,self).__init__();
+
+    def do_HEAD(self,s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-    def do_GET(s):
+    def do_GET(self,s):
+        print("HTTP Request Recieved")
         """Respond to a GET request."""
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -28,17 +35,47 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if len(variablesseparate) > 1:
                 var1 = variablesseparate[0].split('=')
                 if var1[0] == 'ipaddr' and len(var1) == 2:
-                    ipaddr = var1[1] 
+                    ipaddr = var1[1]
                 var2 = variablesseparate[1].split('=')
                 if var2[0] == 'macaddr' and len(var2) == 2:
                     macaddr = var2[1]
+                studname = "STUDENT"
+                self.db.addDevice(macaddr,ipaddr,studname)
             s.wfile.write("ipaddr="+ipaddr+",macaddr="+macaddr)
-                    
-        if s.path[:14] == '/histogramdata':
-                s.wfile.write("{ \"resolution\":10,")
-                s.wfile.write("\"frequencies\":[0,0,0,0,3,5,5,15,9,1],")
-                s.wfile.write("\"numStudents\": 38 }")
-            
+
+        if s.path[:14] == "/histogramdata":
+
+                #get list of latest ratings
+                maxtime = self.db.get_max_time()
+                studentratings = self.db.get_time_ratings(maxtime)
+                ratingslist = studentratings.split("\n")[1:]
+                numstudents = len(ratingslist)
+
+                #set number of bars on graph
+                resolution = 10
+
+                frequencylist = []
+
+                #tally up frequency
+                increment = numstudents/resolution
+
+                for currentrating in ratingslist:
+                    lownum = 0
+                    i = 0;
+                    while i < resolution:
+                        highnum = lownum + increment
+                        if currentrating <= highnum and currentrating > lownum:
+                            frequencylist[i] += 1
+                        i += 1
+                        lownum = highnum
+
+
+
+                s.wfile.write("{ \"resolution\":"+resolution+",")
+                s.wfile.write("\"frequencies\":"+frequencylist+",")
+                s.wfile.write("\"numStudents\":"+numstudents+" }")
+
+
         if s.path == '/' or s.path == '':
                 #s.wfile.write("the index.html should be printed out now")
                 f = open("index.html","r")
@@ -49,10 +86,13 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
+    print "Web Server Started"
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((server_host, server_port), RequestHandler)
+    print "httpd initialized"
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+    print "server closed"
