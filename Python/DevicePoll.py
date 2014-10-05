89129a2b4218efebcab328af8327a34345212b2b
omc8db <omc8db@mst.edu>
2014-10-05 03:42:06 -0500

    Update DevicePoll for device discovery framework

--- Python/DevicePoll.py
+++ Python/DevicePoll.py
@@ -9,7 +9,8 @@ import time
 import urllib2 as url
 import dbClient
 PIPE_ID = 0
-POLL_DELAY = 5   #Seconds to delay while polling
+POLL_DELAY = 3   #Seconds to delay while polling
+DEV_LIST_REFRESH_INTERVAL = 10
 
 class devicePoll:
     conn = None
@@ -21,25 +22,32 @@ class devicePoll:
         #get unique primary key
         all_events = self.conn.get_all_events()
         if(all_events):
-            primekey = all_events[len(all_events)][0]   #fetches the last event
+            self.primekey = all_events[len(all_events)][0]   #fetches the last event
     def infpoll(self):
         while (1):
-            self.poll()
-            x = self.conn.read()
-            if x:
-                devices.append(x)
-            time.sleep(POLL_DELAY)
+            for i in range(0,DEV_LIST_REFRESH_INTERVAL):
+                self.poll()
+                time.sleep(POLL_DELAY)
+            self.get_device_db()
     def poll(self):
         curr_time = time.time()
         for device in self.devices:
             val = talk(device)
             self.conn.sendEvents([primekey, val, curr_time, device.owner])
-            primekey = primekey + 1
+            self.primekey = self.primekey + 1
     def talk(device):
         #make http request to device
         f = url.urlopen(device.ip)
         rating = f.read()
         return rating
+
+    ##Updates current device list from DB
+    def get_device_db(self):
+        self.devices = []
+        device_db = self.conn.get_all_devices()
+        for entry in device_db:
+            self.devices.append(device(entry))
+        return self.devices
 
 class device:
     ip = None
